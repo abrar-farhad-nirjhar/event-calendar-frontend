679db23c28a0b61259288c1e95352b4f557a81cf
Abrar Farhad <developer.cwork@gmail.com>
2020-03-16 21:36:59 +0600

    project setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, Button} from 'react-bootstrap'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-    </div>
-  );
+class App extends React.Component{
+  
+  
+  render(){
+    return (
+      <div>
+        <h1>hello world</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
